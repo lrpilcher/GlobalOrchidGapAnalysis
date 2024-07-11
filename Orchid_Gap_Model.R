f20cc9b1e790667eedc33010734524c33e2cdae0
lrpilcher <laurenrosepilcher@outlook.com>
2024-07-11 12:05:12 +0100

    Add files via upload

--- Orchid_Gap_Model.R
+++ Orchid_Gap_Model.R
@@ -1,73 +1,92 @@
-#Running Models to predict conservation status
-#1 - Load Packages - Tidymodels and dplyr packages
+#Load Packages - Tidymodels and dplyr packages
 library(dplyr)
 library(tidymodels)
 
-#2 - Load Data 
-orchiddata <- read.csv2()
+#Load Data 
+orchiddata <- read.csv("Orchid_Model_Data.csv")
 
-#3 - Split Data into Training and Testing Data
-# Create data split object
+#converting outcome variable to a factor
+orchiddata$seedbank_conserved <- as.factor(orchiddata$seedbank_conserved)
+
+
+#Data Re sampling - creating training and test datasets
 orchid_split <- initial_split(orchiddata, prop = 0.75,
-                               strata = tbc)
+                               strata = seedbank_conserved)
 
-# Create the training data
+#Create the training data
 orchid_training <- orchid_split %>% 
   training()
 
-# Create the test data
+#Create the test data
 orchid_test <- orchid_split %>% 
   testing()
 
-# Check the number of rows
+
+#Check the number of rows
 nrow(orchid_training)
 nrow(orchid_test)
 
-#4 - Fitting logistic regression model
 
-# Specify a logistic regression model
-logistic_model <- logistic_reg() %>% 
-  # Set the engine
+#Fitting logistic regression model
+#Specify a logistic regression model
+logistic_model <- logistic_red() %>% 
   set_engine('glm') %>% 
-  # Set the mode
   set_mode('classification')
 
-# Fit to training data
+#Building the Feature Engineering Pipeline
+#Specify Feature Engineering Steps with 'Recipes'.
+orchids_recipe <- recipe(seedbank_conserved ~ region + lifeform_category + climate_description +
+                           threat_pred + redlist_category,
+                         data = orchid_training) %>%
+  step_corr(all_numeric(), threshold = 0.9) %>%
+  step_normalize(all_numeric()) %>%
+  step_dummy(all_nominal(), -all_outcomes())
+
+
+#Recipe training
+orchid_recipe_prep <- orchids_recipe %>%
+  prep(training = orchid_training)
+
+#Preprocess training data
+#Apply our trained recipe to the training data and store results in orchid_training_prep
+orchids_training_prep <- orchid_recipe_prep %>%
+  bake(new_data = NULL)
+orchids_training_prep
+
+#Preprocess test data
+orchids_test_prep <- orchid_recipe_prep %>%
+  bake(new_data = orchid_test)
+orchids_test_prep
+
+
+#Model fitting and predictions
+# Fit to training data using preprocessed training data
 logistic_fit <- logistic_model %>% 
-  fit(conservation_status ~ predictor1 + predictor2 + predictor3,
-      data = orchid_training)
+  fit(seedbank_conserved ~ region + lifeform_category + climate_description +
+        threat_pred + redlist_category,
+      data = orchids_training_prep)
 
-# Print the model specification
-logistic_model
-
-# Predict outcome categories
-class_preds <- predict(logistic_fit, new_data = orchid_test,
+# Obtain model predictions
+class_preds <- predict(logistic_fit, new_data = orchids_test_prep,
                        type = 'class')
 
-# Obtain estimated probabilities for each outcome value
-prob_preds <- predict(logistic_fit, new_data = orchid_test_test, 
+prob_preds <- predict(logistic_fit, 
+                      new_data = orchids_test_prep, 
                       type = 'prob')
 
-# Combine test set results
-orchid_results <- orchid_test %>% 
-  select(tbc) %>% 
-  bind_cols(prob_preds,class_preds)
 
-# View results tibble
-orchid_results
-#Above can be put into a confusion matrix 
+#combine actual outcome variable from test dataset
+orchid_results <- orchid_test %>%
+  select(seedbank_conserved) %>%
+  bind_cols(class_preds, prob_preds)
+
+#produces model results dataframe
+orchid_results 
+
+#model evaluation
+#confusion matrix 
+orchid_results %>%
+  conf_mat(truth = seedbank_conserved, estimate = .pred_class)
 
 
-# Create a custom metric function
-orchid_metrics <- metric_set(accuracy, sens, spec)
 
-# Calculate metrics using model results tibble
-orchid_metrics(orchid_results, truth = tbc,
-                estimate = .class_preds)
-
-# Create a confusion matrix
-conf_mat(orchid_results,
-         truth = tbc,
-         estimate = .class.preds) %>% 
-  # Pass to the summary() function
-  summary()
